94e455d8809b6e13268314f70e1b61d5accb9944
Akhmad <akhmadshaleh@gmail.com>
2023-10-11 18:39:20 +0700

    bug fix

--- resources/views/livewire/post/post-table.blade.php
+++ resources/views/livewire/post/post-table.blade.php
@@ -1,11 +1,14 @@
 <div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Post') }}
+            {{ __('Post Manager') }}
         </h2>
     </x-slot>
 
     <x-hyco.flash-alert />
+    <div wire:loading.delay class="fixed top-0">
+        <x-hyco.loading />
+    </div>
 
     {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 ml-1"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" /></svg>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 ml-1"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" /></svg> --}}
@@ -60,6 +63,10 @@
             <td colspan="100" class="text-center py-10">No data</td>
         </tr>
         @endforelse
+
+        <x-slot name="footer">
+            {{ $posts->links() }}
+        </x-slot>
     </x-hyco.table>
 
     {{-- <div class="py-12">
