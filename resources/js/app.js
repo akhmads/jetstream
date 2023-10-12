58a8ec8051ce604fe06d7f2d371b8b7433106657
Akhmad <akhmadshaleh@gmail.com>
2023-10-12 16:48:45 +0700

    remove markdown editor

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,1 @@
 import './bootstrap';
-import Editor from '@toast-ui/editor'
-import '@toast-ui/editor/dist/toastui-editor.css';
-
-document.addEventListener('livewire:init', () => {
-    if( document.getElementById("editor") ){
-        const editor = new Editor({
-            el: document.querySelector('#editor'),
-            previewStyle: 'vertical',
-            height: '500px',
-            events: {
-                change: function(body,a) {
-                    alert(a);
-                }
-            },
-            initialValue: ''
-        });
-    }
-});
