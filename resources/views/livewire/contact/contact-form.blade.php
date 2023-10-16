10517c249ea89935792ab076855cfeb7b0f1136a
Akhmad <akhmadshaleh@gmail.com>
2023-10-16 14:25:14 +0700

    add form section

--- resources/views/livewire/contact/contact-form.blade.php
+++ resources/views/livewire/contact/contact-form.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Contact') }}
+            {{ __('Contact Manager') }}
         </h2>
     </x-slot>
 
@@ -9,8 +9,43 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <form wire:submit.prevent="store">
 
+            <x-hyco.form-section submit="store">
+                <x-slot name="title">
+                    {{ __('Contact Information') }}
+                </x-slot>
+
+                <x-slot name="description">
+                    {{ __('Update your contact information and email address.') }}
+                </x-slot>
+
+                <x-slot name="form">
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="name" :value="__('Name')" class="mb-1" />
+                        <x-input id="name" wire:model="name" class="w-full" autofocus />
+                        <x-input-error class="mt-2" for="name" />
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="email" :value="__('Email')" class="mb-1" />
+                        <x-input id="email" wire:model="email" class="w-full" />
+                        <x-input-error class="mt-2" for="email" />
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="mobile" :value="__('Mobile')" class="mb-1" />
+                        <x-input id="mobile" wire:model="mobile" class="w-full" />
+                        <x-input-error class="mt-2" for="mobile" />
+                    </div>
+                </x-slot>
+
+                <x-slot name="actions">
+                    <x-hyco.link href="{{ route('contact') }}" wire:navigate icon="x-mark" class="bg-yellow-500 hover:bg-yellow-400">Back</x-hyco.link>
+                    <x-hyco.button wire:loading.attr="disabled" icon="check">Save</x-hyco.button>
+                </x-slot>
+            </x-form-section>
+
+            {{-- <form wire:submit.prevent="store">
                 <div class="mb-4">
                     <x-label for="name" :value="__('Name')" class="mb-1" />
                     <x-input id="name" wire:model="name" class="w-full" autofocus />
@@ -33,8 +68,8 @@
                     <x-hyco.link href="{{ route('contact') }}" wire:navigate icon="x-mark" class="bg-yellow-500 hover:bg-yellow-400">Back</x-hyco.link>
                     <x-hyco.button wire:loading.attr="disabled" icon="check">Save</x-hyco.button>
                 </div>
+            </form> --}}
 
-            </form>
         </div>
     </div>
 </div>
