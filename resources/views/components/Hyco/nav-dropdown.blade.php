6af1db36919c85d03d9c3d01cf52177d6a55bc6a
Akhmad <akhmadshaleh@gmail.com>
2023-12-11 15:18:17 +0700

    fix z-index on navbar

--- resources/views/components/Hyco/nav-dropdown.blade.php
+++ resources/views/components/Hyco/nav-dropdown.blade.php
@@ -1,6 +1,6 @@
 @props(['title','icon' => true,'active' => false])
 
-<div @click.away="open = false" class="relative z-40" x-data="{ open: false }">
+<div @click.away="open = false" class="relative z-50" x-data="{ open: false }">
 
     @if( $active )
     <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-medium text-gray-500 text-left bg-gray-200 rounded-lg dark-mode:bg-gray-200 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline delay-50 duration-300 ease-in-out">
@@ -15,7 +15,7 @@
         </svg>
         @endif
     </button>
-    <div x-cloak x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="relative lg:absolute right-0 w-full mt-2 origin-top-right rounded-md lg:shadow-lg md:w-48">
+    <div x-cloak x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="relative z-50 lg:absolute right-0 w-full mt-2 origin-top-right rounded-md lg:shadow-lg md:w-48">
         <div class="px-2 py-2 bg-white rounded-md lg:shadow dark-mode:bg-gray-800">
             {{ $slot }}
         </div>
