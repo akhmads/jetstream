14ec1a1c3d07b580ff893b0aa65c11a741e5fded
Akhmad <akhmadshaleh@gmail.com>
2023-12-21 15:38:40 +0700

    update bank transaction models

--- app/Models/BankTransDetail.php
+++ app/Models/BankTransDetail.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class CashTransDetail extends Model
+class BankTransDetail extends Model
 {
     use HasFactory;
 
-    protected $table = 'cash_trans_detail';
+    protected $table = 'bank_trans_detail';
     protected $guarded = [];
     protected $casts = [];
 }
